35715971831d85d47cc61d302503b5891cc5d3b6
vipul <vipulvaghela1988@gmail.com>
2016-07-07 12:10:17 +0530

    Implements flicker

--- www/js/controllers/homeController.js
+++ www/js/controllers/homeController.js
@@ -2,22 +2,43 @@
 
 var homeController = function ($scope, $timeout, $interval) {
     $scope.userNames = [
-        {id: 1, name: "vipul"},
-        {id: 2, name: "hitesh"},
-        {id: 3, name: "dk"},
+        {id: 6, name: "vipul"},
+        {id: 3, name: "hitesh"},
+        {id: 2, name: "dk"},
         {id: 4, name: "jigo"},
         {id: 5, name: "nihar"},
-        {id: 6, name: "amit"}
+        {id: 1, name: "amit"}
     ]
     var counter = 0;
-    $scope.isStart=true;
+    var stop;
+
+    $scope.isStart = true;
     $scope.setName = function (name) {
         $scope.randomUser = $scope.userNames[counter];
-        counter++;
+        if (counter === $scope.userNames.length) {
+            counter = 0;
+        } else {
+            counter++;
+        }
+
     };
+    $scope.isDrawInProgress = false;
     $scope.startDraw = function () {
-        $interval($scope.setName, 100, $scope.userNames.length);
+        $scope.isDrawInProgress = true;
+        stop = $interval($scope.setName, 100);
     };
+    $scope.stopDraw = function () {
+        if (angular.isDefined(stop)) {
+            $interval.cancel(stop);
+            $scope.isDrawInProgress = false;
+            stop = undefined;
+        }
+
+    };
+    $scope.$on('$destroy', function () {
+        // Make sure that the interval is destroyed too
+        $scope.stopDraw();
+    });
 
 };
 angular.module('plc.controllers')
